767da15a9b8e5a00d3e0278200d2f607a4c820a5
yinglongyhy <yhy18218945272@outlook.com>
2018-08-01 18:59:12 +0800

    add findChar.java

---
+++ Java-2/src/com/scau/findChar.java
@@ -0,0 +1,33 @@
+package com.scau;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class findChar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        String pattern = input.next();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            int count = 1;
+            char ch = pattern.charAt(i);
+            i++;
+            while (count < n && i < pattern.length()) {
+                if (ch == pattern.charAt(i)) {
+                    i++;
+                    count++;
+                } else {
+                    i--;
+                    break;
+                }
+            }
+            if (count >= n) {
+                System.out.println(ch);
+                return;
+            }
+        }
+        System.out.println("No");
+    }
+}
